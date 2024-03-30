e65223d7294b6eac2ffebd6095f5e1fe9d90f310
yadivyanshu <90229409+yadivyanshu@users.noreply.github.com>
2024-03-31 03:19:55 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,7 @@ import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import Checkout from './Checkout';
+import emailjs from '@emailjs/browser';
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
@@ -28,18 +29,80 @@ const Cart = (props) => {
   };
 
   const submitOrderHandler = async (userData) => {
+    debugger; 
     setIsSubmitting(true);
-    await fetch('https://fooddelivery-b4a1b-default-rtdb.firebaseio.com/orders.json', {
-      method: 'POST',
-      body: JSON.stringify({
-        user: userData,
-        orderedItems: cartCtx.items,
-      }),
-    });
-    setIsSubmitting(false);
-    setDidSubmit(true);
-    cartCtx.clearCart();
+  
+    try {
+      const response = await fetch('https://fooddelivery-b4a1b-default-rtdb.firebaseio.com/orders.json', {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch orders.');
+      }
+  
+      const orderId = await response.json();
+  
+      const orderDetails = await getOrderDetails(orderId); 
+  
+      let totalPrice = orderDetails.orderedItems.reduce((acc, item) => {
+        return acc + (item.price * item.amount);
+      }, 0); 
+  
+      const formattedOrderItems = orderDetails.orderedItems.map(item => {
+        return `- ${item.name} (${item.amount}) x $${item.price.toFixed(2)}\n`;
+      });
+  
+      const orderItemsString = formattedOrderItems.join('');
+      const templateParams = {
+        to_email: orderDetails.user.Email,
+        to_name: orderDetails.user.name,
+        orders: orderDetails.orderedItems,
+        subject: 'Order Confirmation',
+        message: `Dear ${orderDetails.user.name},\n\nCongratulation! Your order has been successfully placed!\n\nYour order details:\n${orderItemsString}\nTotal amout to be paid: $${totalPrice.toFixed(2)}\n\nIt will be arriving in ${Math.floor(Math.random() * 10) + 20} mins.`,
+      };
+  
+      const serviceId = 'service_m171dfw';
+      const templateId = 'template_nlln0vc';
+      const userId = 'fuZgQg5wPwD7BRxaJ';
+  
+      emailjs.send(serviceId, templateId, templateParams, userId)
+        .then((response) => {
+          console.log('Email sent successfully!', response);
+        })
+        .catch((error) => {
+          console.error('Error in sending email: ', error);
+        });
+  
+    } catch (error) {
+      console.error('Error submitting order:', error);
+    } 
+    finally {
+      setIsSubmitting(false);
+      setDidSubmit(true);
+      cartCtx.clearCart();
+    }
   };
+  
+  async function getOrderDetails(orderId) {
+    try {
+      const response = await fetch(`https://fooddelivery-b4a1b-default-rtdb.firebaseio.com/orders/${orderId.name}.json`);
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch order details.');
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching order details:', error);
+    }
+  }
+
 
   const cartItems = (
     <ul className={classes['cart-items']}>
